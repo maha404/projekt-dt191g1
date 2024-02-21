b0cc6b04f911be44c008f713189fda2168d82e16
Your Name <you@example.com>
2024-02-21 11:43:48 +0100

    Skapade ett mvc projekt.

---
+++ AdminInterface/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AdminInterface.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# projekt-dt1919g
